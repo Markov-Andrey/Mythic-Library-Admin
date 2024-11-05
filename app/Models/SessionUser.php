6151a13293f00414a8289f5a8f84445a53d814e6
Andre Markov <andremarkov@icloud.com>
2024-11-05 11:22:04 +0300

    add session users

--- app/Models/SessionUser.php
+++ app/Models/SessionUser.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
+/**
+ * Игроки в сессии
+ */
 class SessionUser extends Pivot
 {
     use HasFactory;
